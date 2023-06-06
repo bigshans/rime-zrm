4a3fcd8e0f7989bbe16e2d7bdfe2ada3613a3168
sharp-tasting <hongchaoliu001@gmail.com>
2023-06-06 23:11:31 +0800

    恢复到三四码置换之前, 因其导致dict过于难用. 并且手动纠正部分辅助码

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,18 +1,18 @@
 '''convert the 3rd code of zrm_pinyin.'''
 
-import string
+# import string
 import os
 from cnradical import Radical, RunOption
 radical = Radical(RunOption.Radical)  # 获取偏旁
 
-bs_map = {}  # 部首 -> 辅助码 的字典
-with open('aux_code.txt', "r", encoding="utf-8") as f0:
-    for li0 in f0:
-        lst = list(li0)  # 拆成单个字符(含空格, 换行符等)
-        for i in lst[1:]:  # 排除首个字符(因其是辅码)
-            if i not in string.whitespace:
-                # 排除掉 whitespace, 剩下的就是部首
-                bs_map[i] = lst[0]
+# bs_map = {}  # 部首 -> 辅助码 的字典
+# with open('aux_code.txt', "r", encoding="utf-8") as f0:
+#     for li0 in f0:
+#         lst = list(li0)  # 拆成单个字符(含空格, 换行符等)
+#         for i in lst[1:]:  # 排除首个字符(因其是辅码)
+#             if i not in string.whitespace:
+#                 # 排除掉 whitespace, 剩下的就是部首
+#                 bs_map[i] = lst[0]
 
 # 用于调整部首的辅助码, item的形式为 'bs_ch':{'orig_ac':'new_ac'}
 bs_convert = {}
@@ -38,10 +38,10 @@ def resh3(src, temp):
             elif bs_ch in bs_convert and line[5] in bs_convert[bs_ch]:
                 line2 = line.replace(';'+line[5],
                                      ';'+bs_convert[bs_ch][line[5]])
-            elif bs_ch in bs_map and line[6] == bs_map[bs_ch]:
-                # 部首被放在第四码, 则置换第三码和第四码
-                line2 = line.replace(';'+line[5]+line[6],
-                                     ';'+line[6]+line[5])
+            # elif bs_ch in bs_map and line[6] == bs_map[bs_ch]:
+            #     # 部首被放在第四码, 则置换第三码和第四码(会导致 dict 文件非常难用)
+            #     line2 = line.replace(';'+line[5]+line[6],
+            #                          ';'+line[6]+line[5])
             else:
                 line2 = line
 
